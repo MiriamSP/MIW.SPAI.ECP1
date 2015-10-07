94c27d7a53956933ef1c17975271a453682545f7
msanzpascua <msanzpascua@aeport1067.acciona.int>
2015-10-07 20:14:03 +0200

    C31Test

---
+++ src/test/java/miriam/sanz/ecp1/miw/upm/es/C31Test.java
@@ -0,0 +1,25 @@
+package miriam.sanz.ecp1.miw.upm.es;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class C31Test {
+
+    private C31 c;
+
+    @Before
+    public void before() {
+        c = new C31();
+    }
+
+    @Test
+    public void testM1() {
+        assertEquals("m1", c.m1());
+    }
+
+    @Test
+    public void testM2() {
+        assertEquals("m2", c.m2());
+    }
+}
