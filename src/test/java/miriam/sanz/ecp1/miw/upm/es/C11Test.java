023d7a02f6f16a62dfd9f888e8be1d32cd6fda78
msanzpascua <msanzpascua@aeport1067.acciona.int>
2015-10-07 20:03:11 +0200

    C11Test

---
+++ src/test/java/miriam/sanz/ecp1/miw/upm/es/C11Test.java
@@ -0,0 +1,28 @@
+package miriam.sanz.ecp1.miw.upm.es;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class C11Test {
+
+    private C11 c;
+
+    @Before
+    public void before() {
+        c = new C11();
+    }
+
+    @Test
+    public void testM1() {
+        assertEquals("m1", c.m1());
+    }
+
+    @Test
+    public void testM2() {
+        assertEquals("m2", c.m2());
+    }
+
+}
